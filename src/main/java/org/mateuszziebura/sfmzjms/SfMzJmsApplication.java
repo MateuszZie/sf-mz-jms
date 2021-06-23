8436a539b85e5554fb84772cc14b6c373c82eb3e
Mateusz Ziebura <mateusz_860@op.pl>
2021-06-23 14:11:16 +0200

    Initial Project and Maven Dependencies

---
+++ src/main/java/org/mateuszziebura/sfmzjms/SfMzJmsApplication.java
@@ -0,0 +1,13 @@
+package org.mateuszziebura.sfmzjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfMzJmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfMzJmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mateuszziebura/sfmzjms/SfMzJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mateuszziebura.sfmzjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfMzJmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
